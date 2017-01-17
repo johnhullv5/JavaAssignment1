58ade7a59b3c252cd60f5052c3f6832741c50d81
300858525 <300858525@PR-A317-22.cencol.ca>
2017-01-17 14:37:08 -0500

    GUI ready.

--- HaoJiang_300858525_COMP303_Assignment1/src/com/rmi/client/BMIClient.java
+++ HaoJiang_300858525_COMP303_Assignment1/src/com/rmi/client/BMIClient.java
@@ -7,12 +7,16 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.text.DecimalFormat;
 
 import com.rmi.demo.*;
 import com.rmi.demo.BMICalculator;
 import com.rmi.demo.BMIConstant;
 
 public class BMIClient {
+	
+	private static DecimalFormat df2 = new DecimalFormat(".##");
+	
 	public static void main(String[] args) throws RemoteException, NotBoundException {
 		try {
 			String height= JOptionPane.showInputDialog("Please input your height(m): ");
@@ -23,13 +27,17 @@ public class BMIClient {
 			Registry registry = LocateRegistry.getRegistry("localhost", BMIConstant.RPORT);
 			BMICalculator h = (BMICalculator) registry.lookup(BMIConstant.RID);
 			
-			JOptionPane.showMessageDialog(null,
-				    "Your BMI: "+h.calculateBMI(height_d, weight_d).toString());
+			Double re = h.calculateBMI(height_d, weight_d);
 			
-			System.out.println("BMI: "+h.calculateBMI(height_d, weight_d));
+			System.out.println("BMI: "+df2.format(re.doubleValue()));
+			JOptionPane.showMessageDialog(null,    "Your BMI: "+df2.format(re.doubleValue()));
+			
+			
 			
 		} catch (Exception e) {
 			System.out.println("BMI client exception: " + e);
+			JOptionPane.showMessageDialog(null,
+				    "Your BMI: N/A");
 		}
 	}
 }
